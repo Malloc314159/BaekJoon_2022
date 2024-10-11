1754b677dc471c262b29420b2a82c4aca0216eac
Jeeyun Choi <65201256+malloc3141@users.noreply.github.com>
2024-10-11 21:09:51 +0900

    [Gold IV] Title: 두 배, Time: 228 ms, Memory: 60904 KB, Score: 100 point -BaekjoonHub

--- 백준/Gold/31963. 두 배/두 배.py
+++ 백준/Gold/31963. 두 배/두 배.py
@@ -4,5 +4,5 @@ n = int(input())
 a = list(map(int, input().split()))
 cnt = [0]*n
 for i in range(1, n):
-    cnt[i] = math.ceil(math.log2(a[i-1]/a[i])) + cnt[i-1]
+    cnt[i] = max(0, math.ceil(math.log2(a[i-1]/a[i])) + cnt[i-1])
 print(sum(cnt))
